b7599a3f8e63f4e57ff17b6d3bf01d192271863b
larsbonde <delesebastian@gmail.com>
2022-02-21 16:17:57 +0100

    final jobbies?

---
+++ scripts/computerome_stuff/final_loo_runs/lstm_cdr_ps_no_swap_rosetta.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+### Every line in this header section should start with ### for a comment
+### or #PBS for an option for qsub
+### Note: No unix commands may be executed until after the last #PBS line
+###
+### Account information
+#PBS -W group_list=ht3_aim -A ht3_aim
+##
+### Send mail when job is aborted or terminates abnormally
+#PBS -M s163691@student.dtu.dk
+#PBS -m abe
+###
+### Compute resources, here 1 core on 1 node
+#PBS -l nodes=1:ppn=4:gpus=1
+###
+### Required RAM in GB
+#PBS -l mem=50GB
+###
+### How long (max) will the job take, here 24 hours
+#PBS -l walltime=70:00:00
+###
+### Output files - not required to be specified
+### Comment out the next 2 lines to use the job id instead in the file names
+#PBS -e /home/projects/ht3_aim/people/sebdel/masters/scripts/computerome_stuff/train_err_nested_lstm_ps.log
+#PBS -o /home/projects/ht3_aim/people/sebdel/masters/scripts/computerome_stuff/train_run_nested_lstm_ps.log
+###
+### Job name - not required to be specified
+### It is often easier just to use the job id instead for recognition
+#PBS -N loo_tl_ps_ros
+###
+### More qsub options can be added here
+
+
+# This part is the real job script
+# Here follows the user commands:
+
+# Load all required modules for the job
+module load tools 
+module load miniconda3/4.10.3
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate /home/projects/ht3_aim/people/sebdel/envs/envs/proteinsolver/
+
+python3 /home/projects/ht3_aim/people/sebdel/masters/scripts/train/lstm_loo_cdr_pep.py -m ps_rosetta -s
--- scripts/generate_data/extract_cdr_pep_from_embedding_repaired_models.py
+++ scripts/generate_data/extract_cdr_pep_from_embedding_repaired_models.py
@@ -41,28 +41,31 @@ data_root = root / "neat_data"
 metadata_path = data_root / "metadata.csv"
 processed_dir = data_root / "processed"
 state_file = root / "state_files" / "e53-s1952148-d93703104.state"
-model_dir = data_root / "raw" / "tcrpmhc"
 full_seq_path = data_root / "full_seqs.fsa"
 
-if args.source == "ps":
+if args.source == "ps_foldx":
+    model_dir = data_root / "raw" / "foldx_repair"
     data = processed_dir / "proteinsolver_embeddings_pos"
     targets = processed_dir / "proteinsolver_embeddings_pos" / "targets.pt"
-    out_dir = processed_dir / "proteinsolver_embeddings_cdr_pep_only"
+    out_dir = processed_dir / "proteinsolver_embeddings_cdr_pep_only_foldx"
 
-if args.source == "esm":
-    data = processed_dir / "esm_embeddings_pos"
-    targets = processed_dir / "proteinsolver_embeddings_pos" / "targets.pt"
-    out_dir = processed_dir / "esm_embeddings_cdr_pep_only"
-
-if args.source == "esm_ps":
+if args.source == "esm_ps_foldx":
+    model_dir = data_root / "raw" / "foldx_repair"
     data = processed_dir / "proteinsolver_esm_embeddings_pos"
     targets = processed_dir / "proteinsolver_embeddings_pos" / "targets.pt"
-    out_dir = processed_dir / "proteinsolver_esm_embeddings_cdr_pep_only"
+    out_dir = processed_dir / "proteinsolver_esm_embeddings_cdr_pep_only_foldx"
 
-if args.source == "blosum":
-    data = processed_dir / "blosum_embeddings_pos"
+if args.source == "ps_rosetta":
+    model_dir = data_root / "raw" / "rosetta_repair"
+    data = processed_dir / "proteinsolver_embeddings_pos"
     targets = processed_dir / "proteinsolver_embeddings_pos" / "targets.pt"
-    out_dir = processed_dir / "blosum_embeddings_cdr_pep_only"
+    out_dir = processed_dir / "proteinsolver_embeddings_cdr_pep_only_rosetta"
+
+if args.source == "esm_ps_rosetta":
+    model_dir = data_root / "raw" / "rosetta_repair"
+    data = processed_dir / "proteinsolver_esm_embeddings_pos"
+    targets = processed_dir / "proteinsolver_embeddings_pos" / "targets.pt"
+    out_dir = processed_dir / "proteinsolver_esm_embeddings_cdr_pep_only_rosetta"
 
 # Get metadata
 paths = list(model_dir.glob("*"))
@@ -94,30 +97,32 @@ out_dir.mkdir(mode=0o775, parents=True, exist_ok=True)
 
 chain_keys = np.array(["P", "M", "A", "B"])
 
-for i, record in enumerate(SeqIO.parse(full_seq_path, "fasta")):
-    seq = np.array(list(record.seq))
-    
-    data = dataset_pre[i]
-    x = dataset_emb[i][0]
-    metadata_row = metadata.iloc[i]
-    
-    new_data = list()
-    for cdr3, tcr in [["CDR3a", "A"], ["CDR3b", "B"]]:
-        # get TCR and CDR3 seq
-        tcr_seq = seq[np.where(data.chain_map == tcr)]
-        cdr3_seq = np.array(list(metadata_row[cdr3]))
+metadata_id_set = set(metadata["#ID"])
+for record in SeqIO.parse(full_seq_path, "fasta"):
+    if record.id in metadata_id_set:
+        i = metadata[metadata["#ID"] == seq_id].index[0]
+        seq = np.array(list(record.seq))
+        data = dataset_pre[i]
+        x = dataset_emb[i][0]
+        metadata_row = metadata.iloc[i]
         
-        # find indices of CDR3 in TCR
-        start_idx, end_idx = find_subset_idx(tcr_seq, cdr3_seq)
+        new_data = list()
+        for cdr3, tcr in [["CDR3a", "A"], ["CDR3b", "B"]]:
+            # get TCR and CDR3 seq
+            tcr_seq = seq[np.where(data.chain_map == tcr)]
+            cdr3_seq = np.array(list(metadata_row[cdr3]))
+            
+            # find indices of CDR3 in TCR
+            start_idx, end_idx = find_subset_idx(tcr_seq, cdr3_seq)
+            
+            # slice embedded data to correct chain using pos encoding and subset CDR indices
+            pos_encoding_key = np.where(chain_keys == tcr)[0][0] - 4 # subtract 4 to get same key as when data was generated
+            cdr3_embed = x[x[:, pos_encoding_key] == 1]  
+            cdr3_embed = cdr3_embed[start_idx:end_idx]
+            new_data.append(cdr3_embed)
         
-        # slice embedded data to correct chain using pos encoding and subset CDR indices
-        pos_encoding_key = np.where(chain_keys == tcr)[0][0] - 4 # subtract 4 to get same key as when data was generated
-        cdr3_embed = x[x[:, pos_encoding_key] == 1]  
-        cdr3_embed = cdr3_embed[start_idx:end_idx]
-        new_data.append(cdr3_embed)
-    
-    pos_encoding_key = np.where(chain_keys == "P")[0][0] - 4
-    peptide_embed = x[x[:, pos_encoding_key] == 1]
-    new_data.append(peptide_embed)
-    new_data = torch.vstack(new_data)
-    torch.save(new_data, out_dir / f"data_{i}.pt")
+        pos_encoding_key = np.where(chain_keys == "P")[0][0] - 4
+        peptide_embed = x[x[:, pos_encoding_key] == 1]
+        new_data.append(peptide_embed)
+        new_data = torch.vstack(new_data)
+        torch.save(new_data, out_dir / f"data_{i}.pt")
